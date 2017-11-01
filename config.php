2b9aab3440e9880f98b15df1b048eb064244f887
priyapawar2 <33164571+priyapawar2@users.noreply.github.com>
2017-11-01 10:50:35 -0700

    Update config.php
    Put system's and database passwords in configuration.

--- config.php
+++ config.php
@@ -3,12 +3,12 @@ ini_set( "display_errors", true );
 date_default_timezone_set( "America/Los_Angeles" );  // http://www.php.net/manual/en/timezones.php
 define( "DB_DSN", "mysql:host=localhost;dbname=cms" );
 define( "DB_USERNAME", "root" );
-define( "DB_PASSWORD", "shriganesh" );
+define( "DB_PASSWORD", "----" );
 define( "CLASS_PATH", "classes" );
 define( "TEMPLATE_PATH", "templates" );
 define( "HOMEPAGE_NUM_ARTICLES", 5 );
 define( "ADMIN_USERNAME", "admin" );
-define( "ADMIN_PASSWORD", "myfirstwebsite" );
+define( "ADMIN_PASSWORD", "-----" );
 require( CLASS_PATH . "/Article.php" );
 
 function handleException( $exception ) {
